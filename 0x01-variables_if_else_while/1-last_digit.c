c9a9c2bf6c1a25f026065ec6a9a8794b54141392
Peter Muema <ptrmutie@gmail.com>
2023-02-17 14:47:21 -0800

    program to store random numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/*
+ * A program for assigning random number to the variable
+*return(0)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n,m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+		m = n % 10;
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+	else if (m < 6 && m != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	}
+	return (0);
+}
