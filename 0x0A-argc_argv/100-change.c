7dabe604ea6d3245708fa267991d48e9371ed563
Peter Muema <ptrmutie@gmail.com>
2023-03-15 17:22:30 -0700

    made correction to advanced tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,73 +1,65 @@
-#include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
+ * calculate_cents - calculate cents
+ * @num: first parameter
+ * Return: coins
  */
-int coinConverter(int i)
+
+int calculate_cents(int num)
 {
-	int count = 0;
+	int coins = 0;
 
-	while (i != 0)
+	while (num)
 	{
-		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
-		else if (i % 25 == 0)
-			i -= 25;
-		else if (i % 10 == 0)
-			i -= 10;
-		else if (i % 5 == 0)
-			i -= 5;
-			else if (i % 2 == 0)
+		if (num >= 25)
 		{
-			if (i % 10 == 6)
-				i -= 1;
-			else
-				i -= 2;
+			num -= 25;
 		}
-		else
-			i -= 1;
-
-		count++;
+		else if (num >= 10)
+		{
+			num -= 10;
+		}
+		else if (num >= 5)
+		{
+			num -= 5;
+		}
+		else if (num >= 2)
+		{
+			num -= 2;
+		}
+		else if (num >= 1)
+		{
+			num -= 1;
+		}
+		coins++;
 	}
-
-	return (count);
+	return (coins);
 }
 
 /**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - print the minimum number of coins
+ * @argc: parameter 1
+ * @argv: array
+ * Return: 0 (success)
  */
-int main(int argc, char *argv[])
-{
-	int i, coin;
 
-	coin = 0;
+int main(int argc, char **argv)
+{
+	int number;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+		return (printf("Error\n"), 1);
 	}
+	number = atoi(argv[1]);
 
-	i = atoi(argv[1]);
-
-	if (i < 0)
-		printf("0\n");
-
-	else
+	if (number < 0)
 	{
-		coin = coinConverter(i);
-
-		printf("%d\n", coin);
+		return (printf("0\n"), 1);
 	}
-
+	printf("%d\n", calculate_cents(number));
 	return (0);
 }
